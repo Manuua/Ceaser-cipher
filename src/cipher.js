552ed62bbc1c1f420335d9089889113fa49cc821
Manuua <manoelaaraujo316@outlook.com>
2022-04-25 15:18:25 -0300

    Cifra de Cesar

--- src/cipher.js
+++ src/cipher.js
@@ -1,5 +1,27 @@
-const cipher = {
-  // ...
-};
+ //const cipher = {cifra};
 
-export default cipher;
+ //export default cifra()
+ export { cifra as default }
+ //export { nome1, nome2, …, nomeN}; para exportat + de uma função 
+ 
+
+function cifra (frase){
+  // the acumullator 
+  let solution = "";
+  // loop through the string 
+  for(let i=0; i< frase.length; i++){
+    let ascii = frase[i].charCodeAt();
+
+     if(ascii >= 65 && ascii <= 77 ){
+        solution += String.fromCharCode(ascii + 13);
+     }else if(ascii >= 78 && ascii <= 90){
+         solution += String.fromCharCode(ascii - 13)
+     } else {
+     solution += frase[i];
+    }   
+  }
+   return document.getElementById('resultPrincipal').innerHTML = 
+   `<div id="resultPrincipal">
+    <p >Sua mensagem é  : ${solution} </p>
+  </div>`;
+}
--- src/index.js
+++ src/index.js
@@ -1,3 +1,39 @@
-import cipher from './cipher.js';
+import cifra from './cipher.js';
 
-console.log(cipher);
+// manipulação do dom feita aqui
+
+//selecting elements 
+const btnToCript = document.getElementById("button1");// button to Cript
+const btnToDescript = document.getElementById("button2"); // Button to Descript
+const textArea = document.getElementById("input");//Area with the msg of user 
+
+// function RTO13
+btnToCript.addEventListener('click', function (){
+    const text = textArea.value.toUpperCase()
+    cifra(text)
+}) 
+
+ btnToDescript.addEventListener('click' , function() {
+    const text = textArea.value.toUpperCase()
+    cifra(text)
+ })
+// função para ativar buttons 
+ const active  = () => {
+    if (document.getElementById("input").length !== 0) {
+      document.getElementById('button1').disabled = false;
+      document.getElementById('button2').disabled = false;
+    } else {
+      document.getElementById('button1').disabled = true;
+      document.getElementById('button2').disabled = true;
+    }
+  };
+// função que desativa botões caso text Area vazio 
+
+ window.onload = () => {
+    document.getElementById('button1').disabled = true;
+    document.getElementById('button2').disabled = true;
+  
+    document.getElementById('input').addEventListener('input', () => {
+      active(btnToDescript,btnToCript);
+    })
+};
