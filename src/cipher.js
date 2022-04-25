4199a244b9e72353a9eae9f9420c7b29f754d743
Manuua <manoelaaraujo316@outlook.com>
2022-04-25 19:56:30 -0300

    Cifra de Cesar

--- src/cipher.js
+++ src/cipher.js
@@ -1,10 +1,6 @@
- //const cipher = {cifra};
-
- //export default cifra()
- export { cifra as default }
- //export { nome1, nome2, …, nomeN}; para exportat + de uma função 
+export { cifra as default }
+//export { nome1, nome2, …, nomeN}; para exportat + de uma função 
  
-
 function cifra (frase){
   // the acumullator 
   let solution = "";
@@ -12,16 +8,18 @@ function cifra (frase){
   for(let i=0; i< frase.length; i++){
     let ascii = frase[i].charCodeAt();
 
-     if(ascii >= 65 && ascii <= 77 ){
+  const FIRST_ASCII_LETTER =65// A 
+  const LAST_OFFSET_WITH_SUM_POSITIVE =77// M 
+  const FIRST_OFFSET_WITH_NEGATIVE_SUM =78// N
+  const LAST_ASCII_LETTER =90// Z
+  
+     if(ascii >= FIRST_ASCII_LETTER  && ascii <= LAST_OFFSET_WITH_SUM_POSITIVE ){
         solution += String.fromCharCode(ascii + 13);
-     }else if(ascii >= 78 && ascii <= 90){
+     }else if(ascii >= FIRST_OFFSET_WITH_NEGATIVE_SUM && ascii <= LAST_ASCII_LETTER){
          solution += String.fromCharCode(ascii - 13)
      } else {
      solution += frase[i];
     }   
   }
-   return document.getElementById('resultPrincipal').innerHTML = 
-   `<div id="resultPrincipal">
-    <p >Sua mensagem é  : ${solution} </p>
-  </div>`;
+   return solution;
 }
--- src/index.js
+++ src/index.js
@@ -7,18 +7,9 @@ const btnToCript = document.getElementById("button1");// button to Cript
 const btnToDescript = document.getElementById("button2"); // Button to Descript
 const textArea = document.getElementById("input");//Area with the msg of user 
 
-// function RTO13
-btnToCript.addEventListener('click', function (){
-    const text = textArea.value.toUpperCase()
-    cifra(text)
-}) 
-
- btnToDescript.addEventListener('click' , function() {
-    const text = textArea.value.toUpperCase()
-    cifra(text)
- })
 // função para ativar buttons 
- const active  = () => {
+// função que desativa botões caso text Area vazio 
+const active  = () => {
     if (document.getElementById("input").length !== 0) {
       document.getElementById('button1').disabled = false;
       document.getElementById('button2').disabled = false;
@@ -27,13 +18,23 @@ btnToCript.addEventListener('click', function (){
       document.getElementById('button2').disabled = true;
     }
   };
-// função que desativa botões caso text Area vazio 
 
- window.onload = () => {
+window.onload = () => {
     document.getElementById('button1').disabled = true;
     document.getElementById('button2').disabled = true;
-  
+
     document.getElementById('input').addEventListener('input', () => {
       active(btnToDescript,btnToCript);
     })
 };
+
+btnToCript.addEventListener('click', function (){
+    const text = textArea.value.toUpperCase()
+    cifra(text)
+
+}) 
+
+ btnToDescript.addEventListener('click' , function() {
+    const text = textArea.value.toUpperCase()
+    cifra(text)    
+ })
